3ba95c772a2d48324c8b0ca2df11bef56daa7cfe
Facturas <eliezelapolinaris2017@gmail.com>
2026-08-04 22:47:59 -0400

    chore: cache visits module

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'neighbor-terra-lugo-v0.5.1';
+const CACHE_NAME = 'neighbor-terra-lugo-v0.6';
 const APP_SHELL = [
   './',
   './index.html',
@@ -12,6 +12,7 @@ const APP_SHELL = [
   './residents-module.js',
   './homes-module.js',
   './vehicles-module.js',
+  './visits-module.js',
   './firebase-config.js',
   './firebase-service.js',
   './manifest.webmanifest',
