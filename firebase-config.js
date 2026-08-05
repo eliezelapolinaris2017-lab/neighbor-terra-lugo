1ceb8bc61df1593ca1bd96a2bfb6247b9fb8abc5
Facturas <eliezelapolinaris2017@gmail.com>
2026-08-04 20:32:07 -0400

    Añadir plantilla de configuración Firebase

---
+++ firebase-config.js
@@ -0,0 +1,12 @@
+// Reemplaza estos valores con la configuración de tu proyecto Firebase.
+// Mientras apiKey permanezca como REEMPLAZAR, Neighbor funcionará en modo local.
+export const firebaseConfig = {
+  apiKey: 'REEMPLAZAR',
+  authDomain: 'REEMPLAZAR.firebaseapp.com',
+  projectId: 'REEMPLAZAR',
+  storageBucket: 'REEMPLAZAR.appspot.com',
+  messagingSenderId: 'REEMPLAZAR',
+  appId: 'REEMPLAZAR'
+};
+
+export const COMMUNITY_ID = 'terra-lugo';
